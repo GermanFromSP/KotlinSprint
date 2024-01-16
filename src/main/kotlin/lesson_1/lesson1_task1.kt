e891974a98efae4580a5bd09108d0d7fa324f0ec
GermanFromSB <polppk84@gmail.com>
2024-01-16 20:51:38 +0300

    KS-1-1 made changes to variables

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,6 +1,6 @@
 package lesson_1
 
 fun main(){
-var numberOfOrders: Int?
-val thanksForPurchase = "Thank you for your purchase, we look forward to seeing you again!"
+val numberOfOrders: Int = 75
+val thanksForPurchase: String = "Thank you for your purchase, we look forward to seeing you again!"
 }
