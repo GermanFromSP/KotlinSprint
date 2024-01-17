66ae7c6f1323bf3e48a2f9489e83054dea829964
GermanFromSB <polppk84@gmail.com>
2024-01-17 22:30:11 +0300

    KS-1-5 created constants

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package lesson_1
 
+private const val SECONDS_IN_HOUR = 3600
+private const val SECONDS_IN_MINUTE = 60
 fun main() {
     val commonTimeInSeconds = 6480
     with(commonTimeInSeconds) {
-        val seconds = this - (this / 60) * 60
-        val minutes = this / 60 - (this / 3600) * 60
-        println("0${this / 3600}:$minutes:0$seconds")
+        val seconds = this - (this / SECONDS_IN_MINUTE) * SECONDS_IN_MINUTE
+        val minutes = this / SECONDS_IN_MINUTE - (this / SECONDS_IN_HOUR) * SECONDS_IN_MINUTE
+        println("0${this / SECONDS_IN_HOUR}:$minutes:0$seconds")
     }
 }
