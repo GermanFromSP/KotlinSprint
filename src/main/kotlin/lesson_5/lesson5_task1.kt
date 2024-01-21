6deb0030a6cb689b8a912208c2e84be1017de093
GermanFromSB <polppk84@gmail.com>
2024-01-21 15:44:38 +0300

    KS-5-1 confirmation of humanity

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -7,10 +7,9 @@ fun main() {
     println("Докажите, что вы не робот.\n17+31=?")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == RIGHT_ANSWER) {
-        println("Добро пожаловать!")
-    } else {
-        println("Доступ запрещен.")
+    when(userAnswer) {
+        RIGHT_ANSWER -> println("Добро пожаловать!")
+        else -> println("Доступ запрещен.")
     }
 
 }
