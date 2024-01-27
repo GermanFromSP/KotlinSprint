30cd0ca9c54c36ef07fce3a04cd94aba28c41c83
GermanFromSP <153195318+GermanFromSP@users.noreply.github.com>
2024-01-27 10:29:08 +0300

    KS-6-2 timer without cycle

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,13 +1,10 @@
 package lesson_6
 
+
 fun main() {
     println("На сколько секунд завести таймер?")
-    val seconds = readln().toInt()
-    var timer = seconds
+    val seconds = readln().toLong()
 
-    while (timer > 0) {
-        timer--
-        Thread.sleep(1000)
-    }
+    Thread.sleep(seconds * 1000)
     println("Прошло $seconds секунд")
 }
