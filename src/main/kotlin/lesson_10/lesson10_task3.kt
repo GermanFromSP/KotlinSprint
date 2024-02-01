9f1e26d80a7b817d40d3540acde49d15d358ebdc
GermanFromSP <153195318+GermanFromSP@users.noreply.github.com>
2024-02-01 12:59:57 +0300

    KS-10-3 generate user password

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,23 @@
+package lesson_10
+
+fun main() {
+    println("Введите длину пароля:")
+    val passwordLength = readln().toInt()
+
+    println("Ваш пароль: ${generatePassword(passwordLength)}")
+}
+
+fun generatePassword(passwordLength: Int): String {
+    val numbers = (0..9).toList()
+    val specialSymbols = ("!\"#$%&'()*+,-./ ").toList()
+    var password = ""
+
+    for (i in 1..passwordLength) {
+        if (i % 2 == 0) {
+            password += specialSymbols[specialSymbols.indices.random()]
+        } else {
+            password += numbers[numbers.indices.random()]
+        }
+    }
+    return password
+}
