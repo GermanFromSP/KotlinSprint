f359f96e9a09203bd0a161b3f3452a99c46aa88c
GermanFromSP <153195318+GermanFromSP@users.noreply.github.com>
2024-02-02 16:50:17 +0300

    KS-10-2 fixed validation function

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -7,12 +7,14 @@ fun main() {
     println("Введите пароль (не менее 4-х символов):")
     val userPassword = readln()
 
-    validateUserData(userName, userPassword)
+    if (!checkDataLength(userName) || !checkDataLength(userPassword)) {
+        println("Логин или пароль недостаточно длинные")
+    } else {
+        println("Добро пожаловать!")
+    }
 }
 
-fun validateUserData(name: String, password: String) {
-    if (name.length < 4 || password.length < 4) {
-        println("Логин или пароль недостаточно длинные")
-    } else
-        println("Добро пожаловать!")
+fun checkDataLength(userParameter: String): Boolean {
+    val minLength = 4
+    return userParameter.length >= minLength
 }
